a9f44d1c24138cac7c814c81f826630c298ea184
ALOK DAS <55158380+alokdas01@users.noreply.github.com>
2022-04-28 17:04:55 +0530

    Add files via upload

---
+++ fetch_poll_data.php
@@ -0,0 +1,49 @@
+<?php
+
+//fetch_poll_data.php
+
+include('database_connection.php');
+
+$php_framework = array("A", "B");
+
+$total_poll_row = get_total_rows($connect);
+$output = '';
+if($total_poll_row > 0)
+{
+	foreach($php_framework as $row)
+	{
+		$query = "SELECT * FROM tbl_poll WHERE php_framework = '".$row."'";
+		$statement = $connect->prepare($query);
+		$statement->execute();
+		$total_row = $statement->rowCount();
+		$percentage_vote = round(($total_row/$total_poll_row)*100);
+		$output .= '
+		<div class="row">
+			<div class="col-md-2" align="right">
+				<label>'.$row.'</label>
+			</div>
+			<div class="col-md-10">
+				<div class="progress">
+					<div class="progress-bar '.$progress_bar_class.'" role="progressbar" aria-valuenow="'.$percentage_vote.'" aria-valuemin="0" aria-valuemax="100" style="width:'.$percentage_vote.'%">
+						'.$percentage_vote.' % programmer like <b>'.$row.'</b> PHP Framework
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		';
+	}
+}
+
+echo $output;
+
+function get_total_rows($connect)
+{
+	$query = "SELECT * FROM tbl_poll";
+	$statement = $connect->prepare($query);
+	$statement->execute();
+	return $statement->rowCount();
+}
+
+
+?>
